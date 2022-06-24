f443f96243e67a44931b74ea33ee2cffa823ec26
DomSmithh <douglas.gon22222@hotmail.com>
2022-06-24 10:41:10 -0400

    Adicionando src/

---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+import "./Title.css";
+
+
+
+function Title(){
+    return(
+        <div>
+            <p class="title">NOTÍCIAS</p>
+            <p class="subtitle">PORTAL DO IFRO</p>
+        </div>
+    );
+}
+
+export default Title;
